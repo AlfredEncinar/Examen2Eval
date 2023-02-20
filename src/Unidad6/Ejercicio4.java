3e060fee5244f2cb6c9e02e271f36d9a3a2a93fa
Alfred Encinar <encinar101@gmail.com>
2023-02-20 16:32:55 +0100

    e

---
+++ src/Unidad6/Ejercicio4.java
@@ -0,0 +1,40 @@
+package Unidad6;
+
+//Escribe un programa que
+// contenga un método que reciba por parámetro el nombre
+// de un fichero de texto (que habrás creado con anterioridad)
+// y lo muestre por pantalla sin espacios en blanco.
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        sinEspacios();
+    }
+
+    public static void sinEspacios(){
+        Scanner sc;
+
+        try (FileReader fr = new FileReader("/home/estudiante/IdeaProjects/Unidad6/hola.txt")) {
+            int leido;
+            sc = new Scanner(fr);
+
+            do {
+                System.out.print(sc.next());
+
+            }while(sc.hasNext());
+
+        } catch (FileNotFoundException e) {
+            System.out.println("no se encuentra el archivo");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+
+
+    }
+}
---
+++ src/Unidad6/Ejercicio5.java
@@ -0,0 +1,33 @@
+package Unidad6;
+
+//Escribe un programa que pida al usuario
+// una cadena de caracteres y la guarde al
+// revés en un archivo de texto cuyo nombre
+// se pasará como segundo argumento.
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa una cadena de caracteres: ");
+        String original = sc.nextLine();
+        String reversed = new StringBuilder(original).reverse().toString();
+
+        System.out.print("Ingresa el nombre del archivo: ");
+        String fileName = sc.nextLine();
+        sc.close();
+
+        try (FileWriter writer = new FileWriter(fileName + ".txt")) {
+            writer.write(reversed);
+            System.out.println("El archivo ha sido guardado con éxito.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo: ");
+        }
+    }
+
+}
